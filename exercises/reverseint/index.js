ecc925ee6aebd0f0a1adfa13d6e06750f3f4b348
farid ghaderi <farid.51gh@gmail.com>
2022-04-24 09:27:34 +0430

    solve reverse int problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//* solution one => convert the input number to string and simply reverse it,
+//* and convert it back to number
+function reverseInt(n) {
+  let number = n.toString().split("").reverse();
+
+  return parseInt(number.join("")) * Math.sign(n);
+}
 
 module.exports = reverseInt;
