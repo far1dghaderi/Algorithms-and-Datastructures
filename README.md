a983e99442412c48c992b9468b229ea55bc5243d
farid ghaderi <farid.51gh@gmail.com>
2022-04-24 09:26:13 +0430

    solve reverse string problem

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # AlgoCasts
 
 Solve most common interview questions and algorithms with various solutions.
+
+this repo's exercises belongs to the following Udemy course by ❤Stephen Grider:
+https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//* #region solution 1
+//convert it to an array, then reverse it easily :D
+/*
+
+function reverse(str) {
+    const charsArray = Array.from(str).reverse();
+    return charsArray.join("");
+}
+*/
+
+//* solution 2
+// get the length of the string, then reverse it with a for loop
+function reverse(str) {
+  let reversedString = "";
+  for (let i = str.length - 1; 0 <= i; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+}
 
 module.exports = reverse;
