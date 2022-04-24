537ca226782fd52dd574ca2bb7142665d36f643c
farid ghaderi <farid.51gh@gmail.com>
2022-04-24 20:47:30 +0430

    solve most used char in string problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//* solution 1 => first of all we'll convert the string to a Set, to remove duplicates
+//* then we use that array of unique chars for iterating through the input string and
+//* counting each character in that string and compare it with others
+
+function maxChar(str) {
+  const usedCharsArray = Array.from(new Set(str));
+  let mostUsedChar = "";
+  let mostUsedCharCount = 0;
+  let charsArray = str.split("");
+  usedCharsArray.forEach((char) => {
+    let currentCharCount = charsArray.filter((cr) => cr == char).length;
+    if (mostUsedCharCount < currentCharCount) {
+      mostUsedChar = char;
+      mostUsedCharCount = currentCharCount;
+    }
+  });
+  return mostUsedChar;
+}
 
 module.exports = maxChar;
