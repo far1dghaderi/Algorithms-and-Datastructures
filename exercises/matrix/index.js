28f3cc8a829ed1e6203fe9911bac166afadc72d4
farid ghaderi <farid.51gh@gmail.com>
2022-04-30 19:05:02 +0430

    solve matrix problem

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let maxNumber = n * n;
+  let matrixArray = [];
+  for (let i = 0; i < n; i++) {
+    matrixArray.push([]);
+  }
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  let counter = 1;
+  while (startCol <= endCol) {
+    //fulfilling the upper rows
+    for (let i = startCol; i <= endCol; i++) {
+      matrixArray[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    //fulfilling the right columns
+    for (let i = startRow; i <= endRow; i++) {
+      matrixArray[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+
+    //fulfilling the bottom rows
+    for (let i = endCol; startCol <= i; i--) {
+      matrixArray[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    //fulfilling the left columns
+    for (let i = endRow; startRow <= i; i--) {
+      matrixArray[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+  }
+  return matrixArray;
+}
+
+console.log(matrix(8).join("\n"));
 
 module.exports = matrix;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//* solution 1 => we iterate through the string and we test each character with our regex pattern
+//* if it passed the test, we add 1 to our vowelsCount
+function vowels(str) {
+  let vowelsCount = 0;
+  str.split("").forEach((char) => {
+    if (RegExp(/a+|e+|i+|o|u/).test(char.toLowerCase())) vowelsCount++;
+  });
+  return vowelsCount;
+}
 
 module.exports = vowels;
